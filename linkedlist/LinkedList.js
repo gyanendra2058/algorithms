8d85a7b41c32e77b001dd0d04ca9bbde528ab97f
Gyanendra Chaubey <gyanendra.chaubey@ge.com>
2019-06-16 13:22:05 +0530

    CCIP 2.1 Solution

--- linkedlist/LinkedList.js
+++ linkedlist/LinkedList.js
@@ -18,6 +18,36 @@ class LinkedList {
         this.tail = nodeAdded;
     }
 
+    /**
+     * CCIP 2.1 Solution
+     */
+    deleteDuplicates() {
+        let traversalMap = new Map();
+        let currPtr = this.head.next;
+        let prevPtr = this.head;
+        traversalMap.set(prevPtr.value, 1);
+
+        while (currPtr !== undefined) {
+            if (!traversalMap.has(currPtr.value)) {
+                traversalMap.set(currPtr.value, 1);
+                prevPtr = prevPtr.next;
+            } else {
+                if (currPtr.next) {
+                    prevPtr.next = currPtr.next;
+                } else {
+                    // Check if tail is redundant and remove it
+                    if (traversalMap.has(currPtr.value)) {
+                        prevPtr.next = undefined;
+                        this.tail = prevPtr;
+                    } else {
+                        this.tail = currPtr;
+                    }
+                }
+            }
+            currPtr = currPtr.next;
+        }
+    }
+
     toStringFormatted() {
         let llString = '';
         let cp = this.head;
--- spec/linkedlist.spec.js
+++ spec/linkedlist.spec.js
@@ -1,13 +1,9 @@
-
 var LinkedList = require('../linkedlist/LinkedList');
-var LLNode = require('../linkedlist/LLNode');
+let newLinkedList;
 
 describe("Linklist test suite", function () {
-    
-    describe("Add behaviour ", function(){
-        var newLinkedList;
-
-        beforeEach(function() {
+    describe("Add behaviour ", function () {
+        beforeEach(function () {
             newLinkedList = new LinkedList(10);
             newLinkedList.addEnd(5);
             newLinkedList.addEnd(15);
@@ -25,7 +21,64 @@ describe("Linklist test suite", function () {
             var results = newLinkedList.toStringFormatted();
             expect(results).toBe('10-5-15-25-35-null');
         });
-         
-    })
-    
+
+    });
+ 
+    describe('Delete behaviour', () => {
+        let deleteDupsCaseLL;
+        beforeEach(() => {
+            deleteDupsCaseLL = new LinkedList(1);
+            deleteDupsCaseLL.addEnd(2);
+            deleteDupsCaseLL.addEnd(3);
+            deleteDupsCaseLL.addEnd(4);
+            deleteDupsCaseLL.addEnd(5);
+        })
+
+        it('should not delete any element if no duplicate elements are present', () => {
+            deleteDupsCaseLL.deleteDuplicates();
+            var results = deleteDupsCaseLL.toStringFormatted();
+            expect(results).toBe('1-2-3-4-5-null');
+        });
+
+        it('should delete element if duplicate elements are present', () => {
+            deleteDupsCaseLL.addEnd(2);
+            deleteDupsCaseLL.addEnd(7);
+            deleteDupsCaseLL.deleteDuplicates();
+            var results = deleteDupsCaseLL.toStringFormatted();
+            expect(results).toBe('1-2-3-4-5-7-null');
+        });
+
+        it('should delete element if many duplicate elements are present', () => {
+            deleteDupsCaseLL.addEnd(15);
+            deleteDupsCaseLL.addEnd(12);
+            deleteDupsCaseLL.addEnd(5);
+            deleteDupsCaseLL.addEnd(15);
+            deleteDupsCaseLL.addEnd(13);
+            deleteDupsCaseLL.addEnd(14);
+            deleteDupsCaseLL.addEnd(13);
+            deleteDupsCaseLL.addEnd(5);
+
+            deleteDupsCaseLL.addEnd(1);
+            deleteDupsCaseLL.addEnd(1);
+            deleteDupsCaseLL.addEnd(2);
+            deleteDupsCaseLL.addEnd(7);
+
+            deleteDupsCaseLL.deleteDuplicates();
+            var results = deleteDupsCaseLL.toStringFormatted();
+            expect(results).toBe('1-2-3-4-5-15-12-13-14-7-null');
+        });
+
+        it('should delete element if redundant tail', () => {
+            deleteDupsCaseLL.addEnd(15);
+            deleteDupsCaseLL.addEnd(12);
+            deleteDupsCaseLL.addEnd(1);
+            deleteDupsCaseLL.addEnd(1);
+            deleteDupsCaseLL.addEnd(2);
+
+            deleteDupsCaseLL.deleteDuplicates();
+            var results = deleteDupsCaseLL.toStringFormatted();
+            expect(results).toBe('1-2-3-4-5-15-12-null');
+        })
+    });
+
 });
